ecef81edd819a038d74454a8d830ddbfd8d6b1ca
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-03-11 23:09:18 +0100

    v1

---
+++ Components/Logo.js
@@ -0,0 +1,7 @@
+const Logo = () => {
+  return (
+    <div className="font-body text-3xl font-bold  text-white">ED OMAR</div>
+  );
+};
+
+export default Logo;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#f4f4f4",
+      },
+      fontFamily: {
+        logo: ["Comfortaa", "cursive"],
+        body: ["Kanit", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
